fabf81c0924de0584b4192f10b4b12d1f10758ab
YukataNinja <YukataNinja@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-31 04:20:15 +0000

    added gnu gpl
    git-svn-id: https://plugins.svn.wordpress.org/ical-for-events-calendar/trunk@90676 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ical-ec.php
+++ ical-ec.php
@@ -6,6 +6,20 @@ Description: Creates an iCal feed for Events Calendar (http://www.lukehowell.com
 Version: 1.0
 Author: Mark Inderhees
 Author URI: http://mark.inderhees.net
+
+---------------------------------------------------------------------
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You can see a copy of GPL at <http://www.gnu.org/licenses/>
+---------------------------------------------------------------------
 */
 
 function iCalFeed()
